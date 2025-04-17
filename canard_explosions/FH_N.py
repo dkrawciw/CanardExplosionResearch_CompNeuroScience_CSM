e6a05d14b1b29f58569248e89cad9bd1c91809a3
Daniel Krawciw <daniel.krawciw@gmail.com>
2025-04-17 14:10:36 -0600

    Added the functions to be used elsewhere

---
+++ canard_explosions/FH_N.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def FH_N(t, y, a=5, tau_n=60, I=-4.25):
+    v, n = y
+    dvdt = v - (v**3) / 3 - n + I
+    dndt = (a * v - n) / tau_n
+
+    return np.array([dvdt, dndt])
---
+++ canard_explosions/__Init__.py
@@ -0,0 +1,2 @@
+from .FH_N import FH_N
+from .HodgkinHuxley import HodgkinHuxley_2D
